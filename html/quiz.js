c6a2aa531907e0b28a0a2542cb9ca9dc3816df40
kalebe <kaleberibeiro@ua.pt>
2022-06-09 10:27:49 +0100

    timer

--- html/quiz.js
+++ html/quiz.js
@@ -1,4 +1,6 @@
 var pergunta = 1;
+var tempo = 20;
+var timer;
 
 function question(){
     pergunta = pergunta + 1;
@@ -6,4 +8,37 @@ function question(){
     if(pergunta == 8){
         console.log("acabou");
     }
+}
+
+
+function updateTimer(){
+    tempo = tempo -1;
+    if(tempo >= 0){
+        document.getElementById("relogio").innerHTML = tempo;
+    }else{
+        console.log("acabou");
+        timerOver();
+    }
+}
+
+function timerOver(){
+    clearInterval(timer);
+}
+
+
+function start(){
+    timer = setInterval(updateTimer, 1000);
+
+    updateTimer();
+}
+
+
+function restartTimer(){
+    timerOver();
+    
+    tempo = 20;
+
+    timer = setInterval(updateTimer, 1000);
+
+    updateTimer;
 }
